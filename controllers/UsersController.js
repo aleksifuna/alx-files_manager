ba48237250a354f61a2c728b5cf67a62c5448a89
Alex Sifuna <aleksifuna@gmail.com>
2024-06-15 02:04:38 +0300

    added a function to add user to db

---
+++ controllers/UsersController.js
@@ -0,0 +1,23 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+export default async function postNew(req, res) {
+  const collection = dbClient.client.db().collection('users');
+  const { email, password } = req.body;
+  if (!email) {
+    res.status(400).json({ error: 'Missing email' });
+    return;
+  }
+  if (!password) {
+    res.status(400).json({ error: 'Missing password' });
+    return;
+  }
+  const user = await collection.findOne({ email });
+  if (user) {
+    res.status(400).json('error: Already exist');
+    return;
+  }
+  const insertionInfo = await collection.insertOne({ email, password: sha1(password) });
+  const userId = insertionInfo.insertedId.toString();
+  res.status(201).json({ email, id: userId });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable import/no-import-module-exports */
 import express from 'express';
 import { getStats, getStatus } from '../controllers/AppController';
+import postNew from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
-
+router.post('/users', postNew);
 module.exports = router;
