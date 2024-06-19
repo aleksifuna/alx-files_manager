654e1782d87ea35a87e81a55a707d171fa9a465d
Alex Sifuna <aleksifuna@gmail.com>
2024-06-19 08:01:14 +0300

    added some changes

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('/', router);
 
 app.listen(PORT, () => {
