9d6d8572d78d2303bf6c5f55d05cf98eef50b2e5
Alex Sifuna <aleksifuna@gmail.com>
2024-06-19 07:21:45 +0300

    tried another way of fixing requiremenr issue

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,15 +1,19 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-export function getStatus(req, res) {
-  if (dbClient.isAlive() && redisClient.isAlive()) {
-    res.status(200).json({ redis: true, db: true });
+class AppController {
+  static getStatus(req, res) {
+    if (dbClient.isAlive() && redisClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    }
+  }
+
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
 
-export function getStats(req, res) {
-  Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-    .then(([usersCount, filesCount]) => {
-      res.status(200).json({ users: usersCount, files: filesCount });
-    });
-}
+export default AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
-/* eslint-disable import/no-import-module-exports */
 import express from 'express';
-import { getStats, getStatus } from '../controllers/AppController';
-import { getConnect, getDisconnect } from '../controllers/AuthController';
-import { postNew, getMe } from '../controllers/UsersController';
-import {
-  getFile, getIndex, getShow, postUpload, putPublish, putUnpublish,
-} from '../controllers/FilesController';
+import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
-router.get('/status', getStatus);
-router.get('/stats', getStats);
-router.get('/users/me', getMe);
-router.post('/users', postNew);
-router.get('/connect', getConnect);
-router.get('/disconnect', getDisconnect);
-router.get('/files/:id', getShow);
-router.get('/files/:id/data', getFile);
-router.put('/files/:id/publish', putPublish);
-router.put('/files/:id/unpublish', putUnpublish);
-router.get('/files', getIndex);
-router.post('/files', postUpload);
-module.exports = router;
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+export default router;
