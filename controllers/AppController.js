4bebf94bee64d14249c5487ec8ca9c8718476a74
Alex Sifuna <aleksifuna@gmail.com>
2024-06-19 20:43:42 +0300

    made some changes to app controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,15 @@
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-class AppController {
-  static getStatus(req, res) {
-    if (dbClient.isAlive() && redisClient.isAlive()) {
-      res.status(200).json({ redis: true, db: true });
-    }
-  }
-
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+export function getStatus(req, res) {
+  if (dbClient.isAlive() && redisClient.isAlive()) {
+    res.status(200).json({ redis: true, db: true });
   }
 }
 
-export default AppController;
+export function getStats(req, res) {
+  Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+    .then(([usersCount, filesCount]) => {
+      res.status(200).json({ users: usersCount, files: filesCount });
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
+import { getStats, getStatus } from '../controllers/AppController';
+import { getConnect, getDisconnect } from '../controllers/AuthController';
+import { postNew, getMe } from '../controllers/UsersController';
+import {
+  getFile, getIndex, getShow, postUpload, putPublish, putUnpublish,
+} from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+router.get('/status', getStatus);
+router.get('/stats', getStats);
+router.get('/users/me', getMe);
+router.post('/users', postNew);
+router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.get('/files/:id', getShow);
+router.get('/files/:id/data', getFile);
+router.put('/files/:id/publish', putPublish);
+router.put('/files/:id/unpublish', putUnpublish);
+router.get('/files', getIndex);
+router.post('/files', postUpload);
 
 export default router;
